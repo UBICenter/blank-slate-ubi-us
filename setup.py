4485e413fb4f8e2711422636f257e1b4b7321abe
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-06-01 13:16:53 +0100

    Add pip package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+   name="blank_slate_ubi_us",
+   version="0.0.1",
+   packages=["blank_slate_ubi_us"],
+   install_requires=[
+       "pandas",
+       "numpy",
+       "scipy",
+       "openfisca-us",
+       "plotly",
+       "ubicenter",
+       "black",
+   ]
+)
